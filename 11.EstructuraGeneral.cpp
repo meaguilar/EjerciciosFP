a72f4b0b3daba6b55ea61a4c2587b8f2aad2f9e3
meaguilar <meaguilar@uca.edu.sv>
2023-04-13 09:22:21 -0600

    :memo:

---
+++ 11.EstructuraGeneral.cpp
@@ -0,0 +1,13 @@
+// Directivas e inclusión de biblioteca iostream
+#include <iostream>
+using namespace std;
+
+// Función principal
+int main()
+{
+
+    // Cuerpo, se incluyen declaraciones,sentencias ...
+
+    // Finalización del programa
+    return 0;
+}
---
+++ 12.VariablesConstTiposDatos.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+// Constante definida
+// #define  <identificador> <valor>
+#define pi 3.1416
+
+int main()
+{
+  // Devolver el tamaño de un tipo de dato o variable
+  sizeof(char);
+
+  // Tipos de datos y declaración de variables
+  // tipoDato <identificador>
+  float var1;
+  int var2;
+  double var3;
+  char var4;
+
+  // Inicializar variable
+  // identificador <valor>
+  var1 = 0;
+
+  // constante declarada
+  const int byte = 8;
+
+  // Salida operador de salida << y componente cout
+  cout << "Uso de componente cout y cin";
+
+  // Entrada operado de inserción >> y componente cin
+  cin >> var2;
+
+  return 0;
+}
